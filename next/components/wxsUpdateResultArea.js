c0f584ba63a309f62c60b05b35d22b6f523c2fd1
juba <balazs.juhasz@klapantius.net>
2018-02-20 15:14:47 +0100

    switched over from List to Card

--- next/components/wxsUpdateResultArea.js
+++ next/components/wxsUpdateResultArea.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Card, CardActions, CardText } from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
 import LinearProgress from 'material-ui/LinearProgress';
 import { List, ListItem } from 'material-ui/List';
+import {Card, CardHeader, CardTitle, CardText} from 'material-ui/Card';
 
 const WxsUpdateResultArea = (props) => (
     <div>
@@ -13,11 +13,11 @@ const WxsUpdateResultArea = (props) => (
             onClick={props.startQuery}
         />
         <LinearProgress style={{ visibility: props.progressIndicator }} mode="indeterminate" />
-        <List>
-            {props.result.map((row, index) => (
-                <ListItem>{row}</ListItem>
+        <Card>
+        {props.result.map((row, index) => (
+                <CardText style={{padding: '1px' }}>{row}</CardText>
             ))}
-        </List>
+        </Card>
     </div>
 )
 
